1b0a6bbbf3bb589c60353d194f700452c95c89eb
Aslam Mahmud Siam <55598241+Amsiam@users.noreply.github.com>
2021-04-04 16:39:56 +0600

    Add files via upload

--- lms/settings.py
+++ lms/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'm#o9*e1y!0eg^3xz0v%0-shqzrd5t@=88-#4aau-_wu5k=6fv#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -121,6 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = BASE_DIR / "static"
 
 
 REST_FRAMEWORK = {
